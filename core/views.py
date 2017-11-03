57c7b235918d535dca1b532ccf9041737845bdc5
nikson <nikson.barth@benner.com.br>
2017-11-03 12:46:07 -0200

    Aula 9

--- core/views.py
+++ core/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    texts = ['lorem ipsum', 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XV']
     context = {
-        'title': 'Django E-commerce'
+        'title': 'Django E-commerce',
+        'texts': texts
     }
     return render(request, 'index.html', context)
-
