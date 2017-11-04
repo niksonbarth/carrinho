adbfdf5637e720d3864b5920891e0c98627381fa
niksonbarth <32281780+niksonbarth@users.noreply.github.com>
2017-11-04 17:58:28 -0200

    teste

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -124,8 +124,12 @@ STATIC_URL = '/static/'
 #    os.path.join(BASE_DIR, 'staticfiles')
 #]
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
+
+DATABASES = {
+    'default':  dj_database_url.config(),
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
