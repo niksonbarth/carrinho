41a7857d8e5b62347882351e78bfab5ffe716c9f
nikson <nikson.barth@benner.com.br>
2017-11-10 13:21:16 -0200

    Aula 24

--- catalog/views.py
+++ catalog/views.py
@@ -6,6 +6,7 @@ def product_list(request):
     context = {
         'product_list': Product.objects.all()
     }
+    
     return render(request, 'catalog/product_list.html', context)
 
 def category(request, slug):
@@ -14,4 +15,13 @@ def category(request, slug):
         'current_category': category,
         'product_list': Product.objects.filter(category=category),
     }
+
     return render(request, 'catalog/category.html', context)
+
+def product(request, slug):
+    product = Product.objects.get(slug=slug)
+    context={
+        'product': product
+    }
+
+    return render(request, 'catalog/product.html', context)
