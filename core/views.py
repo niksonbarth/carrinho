fa5593f87b5ffa0e92edf0c4ccc9ee033eee65d6
nikson <nikson.barth@benner.com.br>
2017-11-14 13:18:03 -0200

    Aula 28

--- core/views.py
+++ core/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from catalog.models import Category
+from .forms import ContactForm
 
 def index(request):
     return render(request, 'index.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    form = ContactForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'contact.html', context)
 
