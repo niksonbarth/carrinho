fd1b961f3196c469019581939916c79073ccdc0c
nikson <nikson.barth@benner.com.br>
2017-11-29 13:03:31 -0200

    Aula 29

--- core/views.py
+++ core/views.py
@@ -7,9 +7,16 @@ def index(request):
     return render(request, 'index.html')
 
 def contact(request):
-    form = ContactForm()
+    success = False
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.send_mail()
+        success = True
+  
     context = {
-        'form': form
+        'form': form,
+        'success': success
     }
+
     return render(request, 'contact.html', context)
 
