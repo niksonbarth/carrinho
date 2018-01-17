3884520701e92e3a545a2cde6a69f4ae763ee740
nikson <niksonbarth@gmail.com>
2018-01-17 12:52:18 -0200

    teste

--- carrinho/settings.py
+++ carrinho/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILE_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
+# STATICFILE_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
