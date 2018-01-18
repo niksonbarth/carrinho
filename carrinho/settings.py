4ea71ac38941180c5cb366613a798bd764286302
nikson <niksonbarth@gmail.com>
2018-01-18 16:39:56 -0200

    login

--- carrinho/settings.py
+++ carrinho/settings.py
@@ -144,6 +144,10 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'admin@carrinho.com'
 
+# auth
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+
 try:
     from .local_settings import *
 except ImportError:
